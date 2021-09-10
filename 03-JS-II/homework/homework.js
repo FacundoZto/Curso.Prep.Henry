8a4380f0cacbb37d221c87cbf55f75c1d4b53923
FacundoZto <facundozerbato@outlook.com>
2021-09-10 08:58:20 -0300

    Tarea 2.1

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -178,7 +178,7 @@ function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-  if (Boolean(valor) === true) {
+  if (valor === true) {
     return 'Soy verdadero';
   } else {
     return 'Soy falso';
